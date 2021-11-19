f6c26f351e614d9032488e8e91564b5ee7a27f40
ceruleanskis <ceruleanskis@protonmail.com>
2021-11-19 00:02:19 -0600

    v0.1.3

--- server/app.py
+++ server/app.py
@@ -4,7 +4,9 @@ from routes import calculate as calculator
 
 app = Flask(__name__)
 
-version = "0.1.2"
+version = "0.1.3"
+
+
 # TODO: Split this route into two; POST /api/calculate and GET /api/warmup
 @app.route('/api/calculate', methods=['POST'])
 def calculate():
