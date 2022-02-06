9dad3e9b61f1ba1ffd813ffa088c66c1449ce28b
ceruleanskis <ceruleanskis@protonmail.com>
2022-02-05 23:10:34 -0600

    Remove conftest

--- server/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-# pylint: skip-file
-def pytest_itemcollected(item):
-    par = item.parent.obj
-    node = item.obj
-    suf = node.__doc__.strip() if node.__doc__ else ''
-    if suf:
-        item._nodeid = suf
